7977d09f8c3f703d16f88b6b6b9ff5f9329ecc07
Martin Zadora <martin.zadora@etu.minesparis.psl.eu>
2025-02-05 23:13:39 +0100

    tests included

--- game_of_life/arguments.py
+++ game_of_life/arguments.py
@@ -7,7 +7,9 @@ def arguments() -> argparse.Namespace:
     DEFAULT_STEPS = 20
     DEFAULT_FPS = 10
     DEFAULT_WIDTH = 40
+    MIN_W, MAX_W = 20, 80
     DEFAULT_HEIGHT = 30
+    MIN_H, MAX_H = 20, 60
 
     parser = argparse.ArgumentParser(description = "Game of Life.",
                                      formatter_class = 
@@ -15,7 +17,7 @@ def arguments() -> argparse.Namespace:
     
     parser.add_argument("--initial_file","-i",type=str,
                         default=DEFAULT_INITIAL_FILE,
-                        help="File from which initial position will be loaded.")
+                        help="File from which initial pattern will be loaded. ")
     
     parser.add_argument("--output_file","-o",type=str,
                         default=DEFAULT_OUTPUT_FILE,
@@ -35,12 +37,16 @@ def arguments() -> argparse.Namespace:
     
     parser.add_argument("--width","-W",type=int,
                         default=DEFAULT_WIDTH,
-                        help="Width of display window.")
+                        help=f"Width of display window, between {MIN_W} an {MAX_W}")
     
     parser.add_argument("--height","-H",type=int,
                         default=DEFAULT_HEIGHT,
-                        help="Height of display window.")
+                        help=f"Height of display window, between {MIN_H} an {MAX_H}")
 
     args = parser.parse_args()
+
+    if (args.width < MIN_W) or ((args.width > MAX_W)) or (args.height < MIN_H) or (args.height > MAX_H):
+        raise ValueError
     
-    return(args)
+    else:
+        return(args)
--- game_of_life/cell_init.py
+++ game_of_life/cell_init.py
@@ -3,11 +3,11 @@ from game_of_life.game import Game
 from game_of_life.state import State
 
 def cell_init(game: Game, live_cells: list[tuple[int,int]]) -> None:
-    for i in range (game.window_width):
-        for j in range(game.window_height):
+    # Increments start at -5 to give the board a buffer zone,
+    # in case patterns evolve outside of the pygame display.
+    for i in range (-5, game.window_width):
+        for j in range(-5, game.window_height):
             if (i,j) in live_cells:
-                cell = Cell(game.tile_size,i,j, State.LIVE)
-                game.add(cell)
+                game.add(Cell(game.tile_size,i,j, State.LIVE))
             else:
-                cell = Cell(game.tile_size,i,j, State.DEAD)
-                game.add(cell)
+                game.add(Cell(game.tile_size,i,j, State.DEAD))
